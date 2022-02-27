9fdad0c53f54938d41406885e7cc33f568e63dc1
YUASDS <1787569211@qq.com>
2022-02-27 12:01:10 +0800

    waifulab

---
+++ saya/AiWaifu/WaifuLab.py
@@ -0,0 +1,183 @@
+import asyncio
+import base64
+from io import BytesIO
+from random import randint
+
+'''width = 1000,
+height = 1000,
+background_color = None,
+mask = mask,
+max_words = 250,
+repeat = True,
+colormap = random.choice(data),
+mode = "RGBA")
+'''
+
+from PIL import Image
+from playwright.async_api import async_playwright,Page
+from playwright._impl import _api_types
+waifu_url = "https://waifulabs.com/generate"
+proxy={"server": "http://127.0.0.1:33210"}
+
+class waifu:
+
+    def __init__(self, url=waifu_url,water_mark=True) -> None:
+        self.url = url
+        self.water_mark=water_mark
+        pass
+    
+    
+    async def gey_browser(self):
+        url = self.url
+        pw = await async_playwright().start()
+        browser = await pw.chromium.launch(
+            headless=False)
+        page = await browser.new_page()
+        try:
+            await asyncio.wait_for(page.goto(url), timeout=2000)
+        except _api_types.TimeoutError:
+            # await browser.close()
+            return None
+        self.page = page
+        self.browser = browser
+        return True
+    
+    async def shot(self,page:Page,ShotSelector:str,WaitSelector:str=None)->BytesIO:
+        if WaitSelector:
+            try:
+                await page.wait_for_selector(WaitSelector, timeout=6000)
+            except asyncio.TimeoutError:
+                print("超时")
+                return
+        await page.wait_for_timeout(3000)
+        shot = await page.query_selector(ShotSelector)
+        buffer = BytesIO(await shot.screenshot(type='jpeg'))
+        return buffer
+
+
+
+    async def first_shot(self, page=None, browser=None)->BytesIO:
+        page = self.page
+        selector = "#wizard-container > div > div > div.waifu-container > div"
+        selector2 = "#wizard-container > div > div > div.waifu-container > div > div:nth-child(16) > div > div > div:nth-child(3)"
+        buffer=await self.shot(page=page,WaitSelector=selector2,ShotSelector=selector)
+        return buffer
+    
+    async def other_shot(self,choose:int=1)->list:
+        page=self.page
+        end_selector="#wizard-container > div > div > div.waifu-preview.shadow.cross-fade-appear-done.cross-fade-enter-done > img"
+        # 下载按钮 
+        selector=f"#wizard-container > div > div > div.waifu-container > div > div:nth-child({choose}) > div > div > div:nth-child(3)"
+        get_page=await page.query_selector(selector)
+        await get_page.click()
+        try:
+            byt_no_water=await self.get_product()
+            await page.wait_for_selector(end_selector,timeout=2000)
+            if self.water_mark:
+                return await self.get_product()
+            else:
+                return byt_no_water
+        except:
+            return await self.page_shot()
+    
+    async def page_shot(self)->tuple:
+        page=self.page
+        selector="#wizard-container > div > div > div.waifu-container > div > div:nth-child(16) > div > div > div:nth-child(3)"
+        ChooseSelector="#wizard-container > div > div > div.waifu-container > div"
+        PicturSelector="#wizard-container > div > div > div.waifu-preview.shadow.cross-fade-enter-done > img"
+        PictureBuffer=await self.shot(page=page,WaitSelector=selector,ShotSelector=PicturSelector) #大图片截图
+        await page.wait_for_timeout(1000)
+        ChooseBuffer=await self.shot(page=page,WaitSelector=selector,ShotSelector=ChooseSelector)     #小图片截图
+        return PictureBuffer,ChooseBuffer
+    
+    async def get_product(self)->BytesIO:
+        '''
+        获取左边的大图片，有延迟就是有水印，无延迟就是无水印
+        '''
+        page=self.page
+        #//*[@id="wizard-container"]/div/div/div[1]/img
+        # selector=f"#wizard-container > div > div > div.waifu-container > div > div:nth-child({choose}) > div > div > div:nth-child(3)"
+        # get_page=await page.query_selector(selector)
+        # await get_page.click()#wizard-container > div > div > div.waifu-container > div > div:nth-child(7) > div > div > div:nth-child(3)
+        # PictureSelector="#wizard-container > div > div > div.waifu-preview.shadow.cross-fade-appear-done.cross-fade-enter-done > img"
+        # pic=await page.wait_for_selector(PictureSelector, timeout=6000)
+        # await page.wait_for_timeout(2000)
+        # img_get=await pic.inner_text()
+        # print(img_get)
+        # ims=Image.open(str_url)
+        # ims.show()
+        # page.wait_for_timeout(500)
+        # imgs=await page.query_selector(imgs_se)
+        # imgs=await imgs.get_attribute("src")
+        # imgs=imgs.split("base64,")[1]
+        # str_url = BytesIO(base64.b64decode(imgs))
+        # ims=Image.open(str_url)
+        # ims.show()
+        # buffer = BytesIO(await pic.screenshot(type='jpeg'))
+        
+        imgs_selector='//*[@id="wizard-container"]/div/div/div[1]/img'
+        imgs=await page.query_selector(imgs_selector)  # 获取图片选择器
+        imgs=await imgs.get_attribute("src")
+        imgs=imgs.split("base64,")[1]
+        buffer = BytesIO(base64.b64decode(imgs))
+        return buffer
+    
+    async def close(self):
+        await self.browser.close()
+        return
+    
+    async def back(self):
+        back_selector="#wizard-container > div > div > div.waifu-preview.shadow.cross-fade-enter-done > div > button:nth-child(1) > span"
+        page=self.page
+        back=await page.query_selector(back_selector)
+        await back.click()
+        return await self.page_shot()
+
+    
+    async def continues(self):
+        
+        continues_selector="#wizard-container > div > div > div.waifu-preview.shadow.cross-fade-enter-done > div > button:nth-child(2) > span"
+        page=self.page
+        continues=await page.query_selector(continues_selector)
+        await continues.click()
+        return await self.page_shot()
+    
+        
+
+
+async def test():
+    waifu_get = waifu(url=waifu_url)
+    s = await waifu_get.gey_browser()
+    byt = await waifu_get.first_shot()
+    img = Image.open(byt)
+    img.show()
+    rd=randint(1,15)
+    byt=await waifu_get.other_shot(rd)
+    img = Image.open(byt[0])
+    img2 = Image.open(byt[1])
+    img.show()
+    img2.show()
+    rd=randint(1,15)
+    byt=await waifu_get.other_shot(rd)
+    img = Image.open(byt[0])
+    img2 = Image.open(byt[1])
+    img.show()
+    img2.show()
+    byt=await waifu_get.other_shot(rd)
+    img = Image.open(byt[0])
+    img2 = Image.open(byt[1])
+    img.show()
+    img2.show()
+    byt=await waifu_get.other_shot(rd)
+    img = Image.open(byt)
+    img.show()
+    pass
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(test())
+
+
+def countSlash(path: str):
+    return path.count("/")
--- saya/AiWaifu/__init__.py
+++ saya/AiWaifu/__init__.py
@@ -1,11 +1,14 @@
 import os
 import random
+import asyncio
 from io import BytesIO
 
 import httpx
 from graia.saya import Saya, Channel
-from graia.ariadne.model import Group
+from graia.ariadne.model import Group,Member
 from graia.ariadne.message.element import Image
+from graia.broadcast.interrupt.waiter import Waiter
+from graia.broadcast.interrupt import InterruptControl
 from graia.ariadne.event.message import GroupMessage
 from graia.ariadne.message.chain import MessageChain
 from graia.saya.builtins.broadcast.schema import ListenerSchema
@@ -13,14 +16,18 @@ from graia.ariadne.message.parser.twilight import Twilight, RegexMatch
 
 from util.sendMessage import safeSendGroupMessage
 from util.control import Permission, Interval, Rest
+from .WaifuLab import waifu
 
 func = os.path.dirname(__file__).split("\\")[-1]
 
 saya = Saya.current()
 channel = Channel.current()
+bcc = saya.broadcast
+inc = InterruptControl(bcc)
 
 http_proxy = {"http://127.0.0.1:33210"}
 
+BROWER=0
 
 async def get_pic(pic_id) -> bytes:
 
@@ -45,9 +52,102 @@ async def get_pic(pic_id) -> bytes:
         ],
     ))
 async def main(group: Group):
-    print("1")
     ran = random.randint(1, 100000)
     img = await get_pic(ran)
     await safeSendGroupMessage(
         group, MessageChain.create("你需要的头像已经生成好了哦~", Image(data_bytes=img)))
+    pass
+
+
+@channel.use(
+    ListenerSchema(
+        listening_events=[GroupMessage],
+        inline_dispatchers=[
+            Twilight({"head": RegexMatch("WaifuLab|Lab头像生成|Lab获取头像|Lab头像"),
+            "water":RegexMatch("无水印",optional=True)
+            })
+        ],
+        decorators=[
+            Permission.require(),
+            Permission.restricter(func),
+            Rest.rest_control(),
+            Interval.require()
+        ],
+    ))
+async def WaifuLab(group: Group,member:Member,water:RegexMatch):
+    global BROWER
+    if BROWER>2:
+        return    await safeSendGroupMessage(
+        group, MessageChain.create("当前进程过多，等一会再来哦~" ))
+    else :
+        BROWER+=1
+    if water.matched:
+        page=waifu(water_mark=False)
+    else:
+        page=waifu()
+    stat=await page.gey_browser()
+    if not stat:
+        BROWER-=1
+        return    await safeSendGroupMessage(
+        group, MessageChain.create("连接超时，等一会再来哦~" ))
+    pic=await page.first_shot()
+    pic=pic.getvalue()
+    await safeSendGroupMessage(
+        group, MessageChain.create("连接成功，请选择下一步哦~" ,Image(data_bytes=pic)))
+    func_dict={
+        "选择":page.other_shot,
+        "继续":page.continues,
+        "返回":page.back,
+        "退出":page.close
+    }
+
+    @Waiter.create_using_function(listening_events=[GroupMessage],
+        using_dispatchers=[
+            Twilight({"head": RegexMatch("选择|继续|返回|退出"),
+                        "choose":RegexMatch("[0-9]+",optional=True)
+            })
+        ]
+        )
+    async def get_waifu(waiter_member:Member,head:RegexMatch,choose:RegexMatch):
+            if member.id==waiter_member.id:
+                head=head.result.asDisplay()
+                if choose.matched:
+                    choose=int(choose.result.asDisplay())
+                    if choose>16:
+                        await safeSendGroupMessage(
+        group, MessageChain.create("超出选择范围了哦~"))
+                        return
+                    if choose==0:
+                        await safeSendGroupMessage(
+        group, MessageChain.create("没有0这个选项哦~"))
+                        return
+                    img=func_dict[head]
+                    img=await img(choose)
+                    if not img:
+                        return False # 当返回为空时，选项为退出
+                    elif isinstance(img,tuple):
+                        await safeSendGroupMessage(
+        group, MessageChain.create("获取成功~",Image(data_bytes=img[0].getvalue()),Image(data_bytes=img[1].getvalue())))
+                        return True
+                    else:
+                        await safeSendGroupMessage(
+        group, MessageChain.create("头像获取成功~",Image(data_bytes=img.getvalue())))
+                        
+                        return False
+                return
+            else:
+                return
+    sign=True
+    while sign:
+        try:
+            sign=await asyncio.wait_for(inc.wait(get_waifu), 30)
+        except asyncio.TimeoutError:
+            await page.close()
+            BROWER-=1
+            return    await safeSendGroupMessage(
+        group, MessageChain.create("等待超时~" ))
+    BROWER-=1
+
+    await safeSendGroupMessage(
+        group, MessageChain.create("你需要的头像已经生成好了哦~"))
     pass
