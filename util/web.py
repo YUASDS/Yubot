509560714f9babd2f46bdc8fef55157236503f7c
YUASDS <1787569211@qq.com>
2022-03-13 10:50:29 +0800

    网络服务util

---
+++ util/web.py
@@ -0,0 +1,28 @@
+from io import BytesIO
+
+import aiohttp
+
+headers={
+
+'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+
+'Cache-Control':'max-age=0',
+
+'Connection':'keep-alive',
+
+'Referer':'http://www.baidu.com/',
+
+'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36 Edg/99.0.1150.39'}
+
+async def get(url:str,type:str="JSON",headers=headers,encoding="utf-8"):
+
+    async with aiohttp.ClientSession() as session:
+        resp = await session.get(url=url,headers=headers)
+        if type=="JSON":
+            return  await resp.json(encoding=encoding)
+        if type=="img":
+            return await BytesIO(resp.content).getvalue()
+        return await resp.text(encoding=encoding)
+        
+
+        
