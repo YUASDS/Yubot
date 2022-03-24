ce773e44e0d815fa094e36253a2b062fd38b8cd7
YUASDS <1787569211@qq.com>
2022-03-24 17:11:10 +0800

    fix bug

--- saya/Sign/__init__.py
+++ saya/Sign/__init__.py
@@ -2,14 +2,13 @@ import os
 from random import randint
 
 from graia.saya import Saya, Channel
-from graia.ariadne.model import Group
-from graia.ariadne.message.element import Member, Image
-from graia.ariadne.event.message import GroupMessage
+from graia.ariadne.message.element import Member, Image, Friend
+from graia.ariadne.event.message import GroupMessage, FriendMessage, MessageEvent
 from graia.ariadne.message.chain import MessageChain
 from graia.saya.builtins.broadcast.schema import ListenerSchema
 from graia.ariadne.message.parser.twilight import Twilight, FullMatch
 
-from util.sendMessage import safeSendGroupMessage
+from util.sendMessage import autoSendMessage
 from util.text2image import font_file
 from util.control import Permission, Interval, Rest
 from database.db import favor, get_info, add_favor, add_gold, is_sign
@@ -25,7 +24,7 @@ channel = Channel.current()
 
 @channel.use(
     ListenerSchema(
-        listening_events=[GroupMessage],
+        listening_events=[GroupMessage, FriendMessage],
         inline_dispatchers=[Twilight(["head" @ FullMatch("签到")])],
         decorators=[
             Permission.require(),
@@ -35,30 +34,32 @@ channel = Channel.current()
         ],
     )
 )
-async def main(group: Group, member: Member):
-    mid = member.id
+async def main(event: MessageEvent):
+    mid = event.sender.id
+    target = event.sender
     if not await is_sign(str(mid)):
-        return await safeSendGroupMessage(
-            group, MessageChain.create("前辈今天已经签到过了哦，等明天再来吧~")
-        )
+        return await autoSendMessage(target, MessageChain.create("前辈今天已经签到过了哦，等明天再来吧~"))
+    favor_add = 1
+    gold_add = randint(1, 15)
+    await add_gold(qq=str(mid), num=gold_add)
+    await add_favor(qq=str(mid), num=favor_add)
     info = await get_info(str(mid))  # 获取信息
     uid = info.id
     favors = info.favor
-    gold_add = randint(1, 15)
-    favor_add = 1
-    await add_gold(qq=str(mid), num=gold_add)
-    await add_favor(qq=str(mid), num=favor_add)
-    favors_now = favors + favor_add
-    fav = favor(favors_now)  # 获取好感度相关数据
+    fav = favor(favors)  # 获取好感度相关数据
     exp = [fav.res, fav.next]
     mahojin_path = os.path.join(path, "imgs", "mahojin.png")
     rewards = [
         Reward(name="乌帕", num=gold_add, ico=os.path.join(path, "imgs", "原石.png")),
         Reward(name="好感度", num=favor_add, ico=os.path.join(path, "imgs", "纠缠之缘.png")),
     ]  # 奖励信息
+    if isinstance(target, Friend):
+        name = target.nickname
+    elif isinstance(target, Member):
+        name = target.name
     pic = await get_signin_img(
         qq=mid,
-        name=member.name,
+        name=name,
         uuid=uid,
         level=fav.level,
         exp=exp,
@@ -67,4 +68,4 @@ async def main(group: Group, member: Member):
         font_path=font_file,
         mahojin_path=mahojin_path,
     )
-    await safeSendGroupMessage(group, MessageChain.create(Image(data_bytes=pic)))
+    await autoSendMessage(target, MessageChain.create(Image(data_bytes=pic)))
