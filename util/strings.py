573c90dcd9b9e2946e4d6e07312ead03ad222c2a
YUASDS <1787569211@qq.com>
2022-07-22 01:23:53 +0800

    另外一种切句

--- util/strings.py
+++ util/strings.py
@@ -1,6 +1,6 @@
 import re
 import string
-
+from PIL.ImageFont import FreeTypeFont
 
 正 = ["一", "丄", "上", "止", "正"]
 L = ["Ⅰ", "Ⅱ", "Ⅲ", "Ⅳ", "Ⅴ", "Ⅵ", "Ⅶ", "Ⅷ", "Ⅸ", "Ⅹ"]
@@ -15,7 +15,7 @@ def numf(num: int):
         return ("%.2f" % (num / 100000000)) + "亿"
 
 
-def get_cut_str(str, cut):
+def get_cut_str(str: str, cut: int) -> list[str]:
     """
     自动断行，用于 Pillow 等不会自动换行的场景
     """
@@ -64,9 +64,49 @@ def get_cut_str(str, cut):
     return non_wrap_str
 
 
+def cut_text(
+    font: FreeTypeFont,
+    origin: str,
+    chars_per_line: int,
+):
+    """将单行超过指定长度的文本切割成多行
+    Args:
+        font (FreeTypeFont): 字体
+        origin (str): 原始文本
+        chars_per_line (int): 每行字符数（按全角字符算）
+    """
+    target = ""
+    start_symbol = "[{<(【《（〈〖［〔“‘『「〝"
+    end_symbol = ",.!?;:]}>)%~…，。！？；：】》）〉〗］〕”’～』」〞"
+    line_width = chars_per_line * font.getlength("一")
+    for i in origin.splitlines(False):
+        if i == "":
+            target += "\n"
+            continue
+        j = 0
+        for ind, elem in enumerate(i):
+            if i[j : ind + 1] == i[j:]:
+                target += i[j : ind + 1] + "\n"
+                continue
+            elif font.getlength(i[j : ind + 1]) <= line_width:
+                continue
+            elif ind - j > 3:
+                if i[ind] in end_symbol and i[ind - 1] != i[ind]:
+                    target += i[j : ind + 1] + "\n"
+                    j = ind + 1
+                    continue
+                elif i[ind] in start_symbol and i[ind - 1] != i[ind]:
+                    target += i[j:ind] + "\n"
+                    continue
+            target += i[j:ind] + "\n"
+            j = ind
+    return target.rstrip()
+
+
 def getCutStr(str, cut):
     si = 0
     i = 0
+    cutStr = ""
     for s in str:
         si += 2 if "\u4e00" <= s <= "\u9fff" else 1
         i += 1
