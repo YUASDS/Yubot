e19f8ced5ec71f38494efb9a6b0bed60ff338879
YUASDS <1787569211@qq.com>
2022-12-31 13:12:27 +0800

    add

--- saya/Identification/__init__.py
+++ saya/Identification/__init__.py
@@ -4,22 +4,20 @@ import random
 
 from pathlib import Path
 from graia.saya import Saya, Channel
-from graia.ariadne.model import Member
-from graia.ariadne.message.element import Source
-from graia.ariadne.event.message import GroupMessage, MessageEvent, FriendMessage
-from graia.saya.builtins.broadcast.schema import ListenerSchema
+from graia.ariadne.event.message import MessageEvent
 from graia.ariadne.message.parser.twilight import (
-    Twilight,
-    FullMatch,
     RegexResult,
 )
 
 from util.sendMessage import autoSendMessage
-from util.control import Permission, Interval
+from util.control import Permission, DaylyLimit
+from util.DaylyRecord import add_data, get_data
 from util.SchemaManager import Schema
 
 path = Path(__file__).parent.joinpath("check.json")
-DATA: list = json.loads(path.read_text(encoding="utf-8"))
+XP_PATH = Path(__file__).parent.joinpath("XP.json")
+CHECK_DATA: list = json.loads(path.read_text(encoding="utf-8"))
+XP: list = json.loads(XP_PATH.read_text(encoding="utf-8"))
 """[qq][date][conten]
 """
 
@@ -30,9 +28,30 @@ channel.name(func)
 schema = Schema(channel)
 
 
+@schema.use(("/XP鉴定"))
+async def xp(event: MessageEvent):
+    res = random.choice(XP)
+    await autoSendMessage(event.sender, f"前辈抽到的XP是：\n【{res}】")
+    """"""
+
+
+@schema.use(("/今日鉴定"))
+async def today(event: MessageEvent):
+    qq = event.sender.id
+    res = get_data(qq, func)
+    await autoSendMessage(event.sender, f"今天的前辈毫无疑问是一只：\n{res}")
+
+
 @schema.use(("/攻受鉴定"))
 async def main(event: MessageEvent):
-    res = random.choice(DATA)
+    qq = event.sender.id
+    func_time = DaylyLimit.limit_dict.get(str(qq), {func: 0}).get(func, 0)
+    if func_time >= 3:
+        res = get_data(qq, func)
+        await autoSendMessage(event.sender, f"今天的前辈毫无疑问是一只：\n{res}")
+        return
+    res = random.choice(CHECK_DATA)
+    add_data(qq, func, res)
     await autoSendMessage(event.sender, f"在千音看来，前辈是一只：\n{res}")
     """"""
 
@@ -41,7 +60,7 @@ async def main(event: MessageEvent):
 async def add(data: RegexResult, event: MessageEvent):
     if data.result:
         res = data.result.asDisplay()
-        DATA.append(res)
+        CHECK_DATA.append(res)
         with open(path, "w", encoding="utf-8") as f:
-            json.dump(DATA, f, ensure_ascii=False, indent=2)
+            json.dump(CHECK_DATA, f, ensure_ascii=False, indent=2)
         return await autoSendMessage(event.sender, f"添加成功！\n{res}")
